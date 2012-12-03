5977dc3f592a243e168d59d26f3aa6883ceb6b5f
Marcel Marti <marmarti@student.ethz.ch>
2012-12-03 17:57:32 +0100

    Added ACK and NACK message.

--- src/ds/sudoku/communication/ACKMessage.java
+++ src/ds/sudoku/communication/ACKMessage.java
@@ -1,14 +1,70 @@
 package ds.sudoku.communication;
 
+import java.util.List;
+import java.util.Map;
+
+import ds.sudoku.communication.serialization.Serializable;
+
 /**
- * Represents a positive confirmation.
+ * Represents a positive confirmation to another message.
  * 
  * <p>
- * Should be used in a clearly defined protocol only.#
+ * Should be used in a clearly defined protocol only.
  * </p>
  * 
  * @author dalhai
  * 
  */
-public class ACKMessage extends Message {
+public class ACKMessage extends TokenizedMessage {
+    
+    private final Message confirmedMessage;
+    
+    /**
+     * Create a new ACKMessage which confirms the given
+     * message. The given message will not be available as
+     * subtype. 
+     * @param confirmedMessage The message to be confirmed.
+     */
+    public ACKMessage(final Message confirmedMessage) {
+        super(null);
+        this.confirmedMessage = confirmedMessage;
+    }
+    
+    /**
+     * Create a new ACKMessage which confirms the given message.
+     * The given message will not be available as subtype. 
+     * The message allows to attach a token.
+     * @param confirmedMessage The message to be confirmed.
+     * @param token The token to be sent with this message.
+     */
+    public ACKMessage(final Message confirmedMessage,
+            Serializable token) {
+        super(token);
+        this.confirmedMessage = confirmedMessage;
+    }
+    
+    /**
+     * Create a new ACKMessage which confirms the given
+     * message. The given message will not be available as
+     * subtype. The message allows to attach a token.
+     * Additionally, store the given custom values and 
+     * properties in the message.
+     * @param confirmedMessage The message to be confirmed.
+     * @param token The token to be sent with this message.
+     * @param customValues The custom values stored in this message.
+     * @param customProperties The custom properties stored in this message.
+     */
+    public ACKMessage(final Message confirmedMessage, Serializable token,
+            List<String> customValues, Map<String, String> customProperties) {
+        super(token, customValues, customProperties);
+        this.confirmedMessage = confirmedMessage;
+    }
+    
+    /**
+     * Get the message that is confirmed with this message. 
+     * @return
+     */
+    public Message getConfirmedMessage() {
+        return confirmedMessage;
+    }
 }
--- src/ds/sudoku/communication/NACKMessage.java
+++ src/ds/sudoku/communication/NACKMessage.java
@@ -1,7 +1,12 @@
 package ds.sudoku.communication;
 
+import java.util.List;
+import java.util.Map;
+
+import ds.sudoku.communication.serialization.Serializable;
+
 /**
- * Represents a negative confirmation.
+ * Represents a negative confirmation to another message.
  * 
  * <p>
  * Should be usined in a clearly defined protocol only.
@@ -10,5 +15,54 @@ package ds.sudoku.communication;
  * @author dalhai
  *
  */
-public class NACKMessage extends Message {
+public class NACKMessage extends TokenizedMessage {
+    
+    private final Message confirmedMessage;
+    
+    /**
+     * Create a new NACKMessage which confirms the given message.
+     * The given message will not be available as subtype.
+     * @param confirmedMessage The message to be confirmed.
+     */
+    public NACKMessage(final Message confirmedMessage) {
+        super(null);
+        this.confirmedMessage = confirmedMessage;
+    }
+    
+    /**
+     * Create a new NACKMessage which confirms the given message.
+     * The given message will not be available as subtype.
+     * The message allows to attach a token.
+     * @param confirmedMessage The message to be confirmed.
+     * @param token The token to be sent with this message.
+     */
+    public NACKMessage(final Message confirmedMessage,
+            Serializable token) {
+        super(token);
+        this.confirmedMessage = confirmedMessage;
+    }
+    
+    /**
+     * Create a new NACKMessage which confirms the given message.
+     * The given message will not be available as subtype.
+     * The message allows to attach a token and additionally,
+     * custom values and properties can be stored.
+     * @param confirmedMessage The message to be confirmed.
+     * @param token The token to be sent with this message.
+     * @param customValues The custom values stored in this message.
+     * @param customProperties The custom properties stored in this message.
+     */
+    public NACKMessage(final Message confirmedMessage, Serializable token,
+            List<String> customValues, Map<String, String> customProperties) {
+        super(token, customValues, customProperties);
+        this.confirmedMessage = confirmedMessage;
+    }
+    
+    /**
+     * Get the message that is confirmed with this message.
+     * @return
+     */
+    public Message getConfirmedMessage() {
+        return confirmedMessage;
+    }
 }
