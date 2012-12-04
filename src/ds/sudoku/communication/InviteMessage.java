ffcd110c8bf968cfce1068e2d8e8403927a5b6da
Marcel Marti <marmarti@student.ethz.ch>
2012-12-04 10:33:49 +0100

    Removed NamedMessage -.-

--- src/ds/sudoku/communication/InviteMessage.java
+++ src/ds/sudoku/communication/InviteMessage.java
@@ -11,43 +11,46 @@ import java.util.Map;
  * @author dalhai
  *
  */
-public class InviteMessage extends NamedMessage {
+public class InviteMessage extends Message {
 	
 	private final String invited;
+	private final String sender;
 	
 	/**
 	 * Creates a new, empty {@link InviteMessage}. This message will
 	 * stand for a request for a random matchup.
-	 * @param from Who sent this invitation?
+	 * @param sender Who sent this invitation?
 	 */
-	public InviteMessage(String from) {
-		this(from, null);
+	public InviteMessage(String sender) {
+		this(sender, null);
 	}
 	
 	/**
 	 * Create a new, empty {@link InviteMessage} from sender {@code from} to
 	 * recipient {@code invited}.
-	 * @param from Who sent this invitation?
+	 * @param sender Who sent this invitation?
 	 * @param invited Who is this invitation for?
 	 */
-	public InviteMessage(String from, String invited) {
-		super(from);
+	public InviteMessage(String sender, String invited) {
+		super();
 		this.invited = invited;
+		this.sender = sender;
 	}
 
 	/**
 	 * Create a new, empty {@link InviteMessage} from sender {@code from} to
 	 * recipient {@code invited}. Additionally, allows storing custom values and
 	 * properties in this message.
-	 * @param from Who sent this invitation?
+	 * @param sender Who sent this invitation?
 	 * @param invited Who is this invitation for?
 	 * @param customValues The custom values stored in this message.
 	 * @param customProperties The custom properties stored in this message.
 	 */
-	public InviteMessage(String from, String invited,
+	public InviteMessage(String sender, String invited,
 			List<String> customValues, Map<String, String> customProperties) {
-		super(from, customValues, customProperties);
+		super(customValues, customProperties);
 		this.invited = invited;
+		this.sender = sender;
 	}
 	
 	/**
@@ -56,5 +59,13 @@ public class InviteMessage extends NamedMessage {
 	 */
 	public String getReciever() {
 		return invited;
+	}
+	
+	/**
+	 * Get the sender of this message.
+	 * @return The sender.
+	 */
+	public String getSender() {
+	    return sender;
 	}
 }
